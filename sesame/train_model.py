dc3d44fb8e9a5a658670bdd2c3d0137fcfad057a
ahmed14-cell <ahmedito14@gmail.com>
2020-11-26 10:26:00 +0300

    Experimenting with Support Vector Machines

--- sesame/train_model.py
+++ sesame/train_model.py
@@ -3,8 +3,7 @@ This is a script to train a model with a variety of estimators
 '''
 import pickle
 import pandas as pd
-from sklearn.tree import DecisionTreeRegressor
-from sklearn.ensemble import AdaBoostRegressor
+from sklearn.svm import SVR
 from config import Config
 
 # Creating a path to save our model
@@ -15,8 +14,7 @@ x_train = pd.read_csv(str(Config.features_path / 'train_features.csv'))
 y_train = pd.read_csv(str(Config.features_path / 'train_target.csv'))
 
 # Instantiating and fitting the algorithm
-model = AdaBoostRegressor(DecisionTreeRegressor(max_depth=4),
-                          random_state=Config.random_seed, n_estimators=100)
+model = SVR(kernel='linear', C=1)
 model = model.fit(x_train, y_train.to_numpy().ravel())
 
 # Saving the model into a pickle file
