f1292b4a31e0d5ade63cca867b82f8b1192febfe
ahmed14-cell <ahmedito14@gmail.com>
2020-11-26 10:10:11 +0300

    Experimenting with Adaboost Regressor

--- sesame/train_model.py
+++ sesame/train_model.py
@@ -4,6 +4,7 @@ This is a script to train a model with a variety of estimators
 import pickle
 import pandas as pd
 from sklearn.tree import DecisionTreeRegressor
+from sklearn.ensemble import AdaBoostRegressor
 from config import Config
 
 # Creating a path to save our model
@@ -14,8 +15,8 @@ x_train = pd.read_csv(str(Config.features_path / 'train_features.csv'))
 y_train = pd.read_csv(str(Config.features_path / 'train_target.csv'))
 
 # Instantiating and fitting the algorithm
-model = DecisionTreeRegressor(max_depth=4,
-                              random_state=Config.random_seed)
+model = AdaBoostRegressor(DecisionTreeRegressor(max_depth=4),
+                          random_state=Config.random_seed, n_estimators=100)
 model = model.fit(x_train, y_train.to_numpy().ravel())
 
 # Saving the model into a pickle file
