d35959714acb4b41c8e97a35dda8babf116d580f
ahmed14-cell <ahmedito14@gmail.com>
2020-12-31 17:07:15 +0300

    refactored code

--- sesame/evaluate_model.py
+++ sesame/evaluate_model.py
@@ -7,21 +7,22 @@ import numpy as np
 import pandas as pd
 from sklearn.metrics import r2_score, mean_squared_error
 from config import Config
+from functions.pipeline import Pipeline as pp
 
 # Loading in our test data for evaluation
 x_test = pd.read_csv(str(Config.features_path / 'test_features.csv'))
 y_test = pd.read_csv(str(Config.features_path / 'test_target.csv'))
 
 # Loading in the trained model
-model = pickle.load(open(str(Config.models_path / 'model.pickle'), 'rb'))
+model = pp.load_pickle(filename='model.pickle', funct='rb')
 
 # Performing predictions on the trained model
 y_pred = model.predict(x_test)
 
 # Calculating metrics for the model (root Mean squared error and R²)
-r2 = r2_score(y_test, y_pred)
-rmse = np.sqrt(mean_squared_error(y_test, y_pred))
+r2 = pp.calculate_r2(ground_truth=y_test, predictions=y_pred)
+rmse = pp.calculate_rmse(ground_truth=y_test, predictions=y_pred)
 
 # Saving our results in a JSON file
-with open(str(Config.metrics_path), 'w') as outfile:
-    json.dump(dict(r_squared=r2, rmse=rmse), outfile)
+pp.dump_json(metric_1=r2, metric_2=rmse)
+
