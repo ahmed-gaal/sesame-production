1db2f69c0011fa61063d993feab7f6accfc62504
ahmed14-cell <ahmedito14@gmail.com>
2020-12-31 18:26:58 +0300

    refactoring code

--- sesame/config.py
+++ sesame/config.py
@@ -1,13 +1,12 @@
 """
-This is a script to provide configuration dependencies for the entire project.
+This is a script to provide configuration 
+dependencies for the entire project.
 """
 from pathlib import Path
 
 
 class Config:
-    """
-    This is a class object to store in configuration details.
-    """
+    """This is a class object to store in configuration details."""
     random_seed = 42
     assets_path = Path('./assets')
     original_data_path = assets_path / 'original_data' / 'dataset.csv'
