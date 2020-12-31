a4ad6f59c2592c9a52fa56560944d62bfac8846f
ahmed14-cell <ahmedito14@gmail.com>
2020-12-31 17:06:16 +0300

    refactored code

--- sesame/train_model.py
+++ sesame/train_model.py
@@ -3,19 +3,21 @@ This is a script to train a model with a variety of estimators
 '''
 import pickle
 import pandas as pd
-from sklearn.neural_network import MLPRegressor
+from sklearn.ensemble import AdaBoostRegressor
 from config import Config
+from functions import Pipeline as pp
 
 # Creating a path to save our model
 Config.models_path.mkdir(parents=True, exist_ok=True)
 
 # Loading the training and testing features into a pandas DataFrame
-x_train = pd.read_csv(str(Config.features_path / 'train_features.csv'))
-y_train = pd.read_csv(str(Config.features_path / 'train_target.csv'))
+x_train = pp.load_features(data='train_features.csv')
+y_train = pp.load_features(data='train_target.csv')
 
 # Instantiating and fitting the algorithm
-model = MLPRegressor(max_iter=800, alpha=0.4371)
+model = AdaBoostRegressor(learning_rate=1.0, loss='square',
+                  n_estimators=50, random_state=42)
 model = model.fit(x_train, y_train.to_numpy().ravel())
 
 # Saving the model into a pickle file
-pickle.dump(model, open(str(Config.models_path / 'model.pickle'), 'wb'))
+pp.dump_pickle(model, filename='model.pickle', funct='wb')
