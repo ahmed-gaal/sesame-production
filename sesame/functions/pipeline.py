5566efb6d830bc59deff90ad3ad912ff21be7f15
ahmed14-cell <ahmedito14@gmail.com>
2020-12-31 18:11:05 +0300

    refactored all methods

--- sesame/functions/pipeline.py
+++ sesame/functions/pipeline.py
@@ -1,3 +1,6 @@
+"""
+This is a script to store all methods used in this workflow.
+"""
 import pickle
 import json
 import pandas as pd
@@ -9,54 +12,61 @@ from config import Config
 
 class Pipeline:
     """
-    Creating a class for all functions in the project
+    Creating a class for all functions in the project.
     """
 
 
-    def __init__():
-        """Initializing the class"""
+    def __init__(self):
+        """Initializing the class."""
 
 
-    def load_features(data=None):
-        '''Function to load features from directory'''
+    def load_features(self, data=None):
+        """Function to load features from directory."""
 
-        data = pd.read_csv(str(Config.features_path / data))
+        data = pd.read_csv(str(Config.features_path / self.data))
         return data
 
 
-    def calculate_r2(ground_truth=None, predictions=None):
-        '''Function to calculate coefficient of determination'''
+    def calculate_r2(self, ground_truth=None, predictions=None):
+        """Function to calculate coefficient of determination."""
 
-        r2 = r2_score(ground_truth, predictions)
+        r2 = r2_score(self.ground_truth, self.predictions)
         return r2
 
 
-    def calculate_rmse(ground_truth=None, predictions=None):
-        '''Function to calculate root mean squared error'''
+    def calculate_rmse(self, ground_truth=None, predictions=None):
+        """Function to calculate root mean squared error."""
 
-        rmse = np.sqrt(mean_squared_error(ground_truth, predictions))
+        rmse = np.sqrt(mean_squared_error(
+            self.ground_truth, self.predictions
+        ))
         return rmse
 
 
-    def load_pickle(filename=None, funct=None):
-        '''Function load pickle file'''
+    def load_pickle(self, filename=None, funct=None):
+        """Function load pickle file."""
 
-        return pickle.load(open(str(Config.models_path / filename), funct))
+        return pickle.load(open(
+            str(Config.models_path / self.filename), self.funct
+        ))
 
 
-    def dump_pickle(model, filename=None, funct=None):
-        '''Function to save model to a pickle file'''
+    def dump_pickle(self, model, filename=None, funct=None):
+        """Function to save model to a pickle file."""
 
-        return pickle.dump(model, open(str(Config.models_path / filename), funct))
+        return pickle.dump(self.model, open(
+            str(Config.models_path / self.filename), self.funct
+        ))
         
         
     def dump_dataset(dataset=None, data=None):
-        '''Function to save pandas DataFrame to csv file'''
+        """Function to save pandas DataFrame to csv file."""
         dataset.to_csv(str(Config.dataset_path / data), index=None)
 
     
-    def dump_json(metric_1=None, metric_2=None):
-        '''Save metrics calculated to a json file'''
+    def dump_json(self, metric_1=None, metric_2=None):
+        """Save metrics calculated to a json file."""
         with open(str(Config.metrics_path), 'w') as outfile:
-            json.dump(dict(r_squared=metric_1, rmse=metric_2), outfile)
-
+            json.dump(
+                dict(r_squared=self.metric_1, rmse=self.metric_2), outfile
+            )
